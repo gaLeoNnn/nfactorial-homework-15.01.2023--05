f9b8cbf650b07f04e90277c7bab24bb0ccffe68c
Ali <Zhanabekov.ali@gmail.com>
2023-01-15 21:56:12 +0600

    HomeWorkLecture5

---
+++ script.js
@@ -0,0 +1,51 @@
+
+
+
+const person = {
+  name: prompt('What is your name', ''),
+  age: prompt('How old are you?', ''),
+  gender: prompt('What is your gender?', '')
+}
+console.log(person);
+
+let b = +prompt('write any number', '');
+if (b == 3) {
+  alert(a);
+} else {
+  alert('Hello');
+}
+
+let a = +prompt('a?', '');
+console.log(a) // string
+// a = +a
+// console.log(a) // number
+
+// if (a == 0) {
+//   alert(0);
+// }
+// if (a == 1) {
+//   alert(1);
+// }
+
+// if (a == 2 || a == 3) {
+//   alert('2,3');
+// }
+switch (a) {
+  case 0:
+    alert(0);
+    break;
+  case 1:
+    alert(1);
+    break;
+  case 2:
+    alert('2,3');
+    break;
+  case 3:
+  alert('2,3');
+  break;
+  default:
+    break;
+}
+
+
+
